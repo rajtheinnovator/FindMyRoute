7a814a152f336c602d49075e6a9a94fd1bb040aa
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-09-16 01:05:35 +0530

    Successfully retrieved routes and marked origin and destination

--- app/src/main/java/com/enpassio/findmyroute/MainActivity.java
+++ app/src/main/java/com/enpassio/findmyroute/MainActivity.java
@@ -1,6 +1,7 @@
 package com.enpassio.findmyroute;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,17 +15,51 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String url = "https://maps.googleapis.com/maps/api/directions/json?";
     int FROM_PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
     int TO_PLACE_AUTOCOMPLETE_REQUEST_CODE = 2;
-    String fromLatLong;
-    String toLatLong;
-
+    Double fromLat;
+    Double fromLong;
+    Double toLat;
+    Double toLong;
+    LatLng toLatLong;
+    String fromLocation;
+    String toLocation;
     TextView fromLocationTextView;
     TextView toLocationTextView;
+    List<Place> placeLists;
+    JSONArray jsonArray;
+    ArrayList<Place> placeArrayLists;
+
+    GoogleMap m_map;
+    boolean mapReady = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +74,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fromPlaceButton.setOnClickListener(this);
         toPlaceButton.setOnClickListener(this);
         routesAvailable.setOnClickListener(this);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -46,10 +84,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode == FROM_PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place: " + place.getName());
-                Log.i(TAG, "Place: " + place.getLatLng());
-                fromLatLong = String.valueOf(place.getLatLng());
-                fromLocationTextView.setText(String.valueOf(place.getLatLng()));
+
+                fromLat = place.getLatLng().latitude;
+                fromLong = place.getLatLng().longitude;
+                fromLocation = place.getName().toString();
+                fromLocationTextView.setText(place.getName().toString());
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO: Handle the error.
@@ -63,10 +102,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (requestCode == TO_PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place: " + place.getName());
-                Log.i(TAG, "Place: " + place.getLatLng());
-                toLatLong = String.valueOf(place.getLatLng());
-                toLocationTextView.setText(String.valueOf(place.getLatLng()));
+
+                toLocation = place.getName().toString();
+                toLocationTextView.setText(place.getName().toString());
+                toLat = place.getLatLng().latitude;
+                toLong = place.getLatLng().longitude;
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO: Handle the error.
@@ -112,8 +152,147 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.routes_available:
+
+
+                MarkerOptions fromLocationMarker = new MarkerOptions().position(new LatLng(fromLat, fromLong)).title(fromLocation);
+                MarkerOptions toLocationMarker = new MarkerOptions().position(new LatLng(toLat, toLong)).title(toLocation);
+
+                m_map.addMarker(fromLocationMarker);
+                m_map.addMarker(toLocationMarker);
+
+                CameraPosition target = CameraPosition.builder().target(new LatLng(fromLat, fromLong)).zoom(18).bearing(90).tilt(60).build();
+                m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
                 //TODO: handle routes using AsyncTask/Retrofit
+                try {
+                    run();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
+    }
+
+    void run() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Uri baseUri = Uri.parse(url);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("origin", "" + fromLat + "," + fromLong);
+        uriBuilder.appendQueryParameter("destination", "" + toLat + "," + toLong);
+        uriBuilder.appendQueryParameter("key", "AIzaSyB-iknh4cmq7Rqtg-lZX1hN124bjxYQGeU");
+
+
+        Request request = new Request.Builder()
+                .url(uriBuilder.toString())
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                /*
+                Instruct GSON to parse as a Post array (which we convert into a list)
+                */
+
+                /** code below referenced from: https://stackoverflow.com/a/29680883/5770629
+                 */
+
+                String jsonData = response.body().string();
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonData);
+
+                    List<List<HashMap<String, String>>> routes = new ArrayList<>();
+                    JSONArray jRoutes;
+                    JSONArray jLegs;
+                    JSONArray jSteps;
+
+                    jRoutes = jsonObject.getJSONArray("routes");
+
+                    /** Traversing all routes */
+                    for (int i = 0; i < jRoutes.length(); i++) {
+                        jLegs = ((JSONObject) jRoutes.get(i)).getJSONArray("legs");
+                        List path = new ArrayList<>();
+
+                        /** Traversing all legs */
+                        for (int j = 0; j < jLegs.length(); j++) {
+                            jSteps = ((JSONObject) jLegs.get(j)).getJSONArray("steps");
+
+                            /** Traversing all steps */
+                            for (int k = 0; k < jSteps.length(); k++) {
+                                String polyline = "";
+                                polyline = (String) ((JSONObject) ((JSONObject) jSteps.get(k)).get("polyline")).get("points");
+                                List<LatLng> list = decodePoly(polyline);
+
+                                /** Traversing all points */
+                                for (int l = 0; l < list.size(); l++) {
+                                    HashMap<String, String> hm = new HashMap<>();
+                                    hm.put("lat", Double.toString((list.get(l)).latitude));
+                                    hm.put("lng", Double.toString((list.get(l)).longitude));
+                                    path.add(hm);
+                                }
+                            }
+                            routes.add(path);
+                        }
+                    }
+
+
+                } catch (JSONException j) {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
+        m_map = googleMap;
+    }
+
+    /**
+     * Method to decode polyline points
+     * Courtesy : https://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
+     */
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
     }
 }
